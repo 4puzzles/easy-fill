ab170d613b2a02affc1401b69f3bf6172f5a9c10
4puzzles <58884766+4puzzles@users.noreply.github.com>
2025-04-13 08:20:58 +0800

    refactor: remove IIFE in the content script
    Since content scripts have their own independent context and execution environment,
    there is no longer a need for IIFE to prevent global variable pollution.

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,25 @@
-// default path to the custom script
-const defaultScriptPath = 'js/inject.js';
+// log current page path
+console.info("%c[Easy Fill]: path '" + location.pathname + "' detected", 'color: green;');
+
+const script = document.createElement('script');
+script.setAttribute('type', 'module');
+
+// cannot use relative path directly like script.src = 'inject.bundle.js'
+// because it violates the following Content Security Policy directive: "script-src 'self'"
+// something like chrome-extension://fhoompeilmbglemidjecngcokdnfojlk/js/inject.js
+script.src = chrome.runtime.getURL('js/inject.js');
+
+// Would you believe that the trash system is running in quirks mode?
+// no doctype, no character set
+// must set charset attribute of the script tag
+// otherwise the chinese text in external javascript file will become garbled
+// charset is a deprecated attribute
+script.charset = 'utf-8';
+
+script.onload = function () {
+  // remove after execution
+  script.parentNode?.removeChild(script);
+};
 
 // inject custom script
-(function(scriptPath) {
-  // log
-  console.info("%c[Easy Fill]: path '" + location.pathname + "' detected", 'color: green;');
-
-	const script = document.createElement('script');
-	script.setAttribute('type', 'module');
-
-  // cannot use relative path directly like script.src = 'inject.bundle.js'
-  // because it violates the following Content Security Policy directive: "script-src 'self'"
-	// something like chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/build/inject.bundle.js
-	script.src = chrome.runtime.getURL(scriptPath);
-
-  // Would you believe that the trash system is running in quirks mode?
-  // no doctype, no character set
-  // must set charset attribute of the script tag
-  // otherwise the chinese text in external javascript file will become garbled
-  // charset is a deprecated attribute
-  script.charset = 'utf-8';
-
-	script.onload = function() {
-		// remove after execution
-		script.parentNode?.removeChild(script);
-	};
-
-	document.head.appendChild(script);
-})(defaultScriptPath);
+document.head.appendChild(script);
